c0b62b47d8479c2f347c81ef0a7f54412d126103
Yashwant Sontakke <yashwant21398@gmail.com>
2020-10-13 21:22:44 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express=require("express")
+const request=require("request")
+const bodyParser=require("body-parser")
+const https = require("https")
+
+const app=express();
+app.use(express.static("public"));//the function is used to add static files to html
+//using css function for that created a one folder and put all the static filesin it and use the path with respect to that folder
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/sign_up.html");
+})
+
+app.post("/", function (req, res){
+  const firstName=req.body.firstName
+  const lastName=req.body.lastName
+  const mailAddress=req.body.mailAddress
+  const data={
+    members:[
+        {
+          email_address: mailAddress,
+          status: "subscribed",
+          merge_fields:{
+            FNAME:firstName,
+            LNAME: lastName
+            }
+        }
+    ]
+  }
+  const jsonData=JSON.stringify(data);
+
+  const url="https://us2.api.mailchimp.com/3.0/lists/080841f0fc";
+  const options = {
+    method: "POST",
+    auth:"yashwant:091a6e74cd271bf5556eb4558feb4e7e-us2"
+    }
+  const request=https.request(url, options, function(response){
+   if(response.statusCode === 200){
+     res.sendFile(__dirname + "/sucess.html")
+   }
+   else{
+     res.sendFile(__dirname + "/failure.html")
+   }
+  });
+  request.write(jsonData);
+  request.end();
+
+
+})
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT||3000,function(){
+  console.log("server has started")
+})
+//key
+//091a6e74cd271bf5556eb4558feb4e7e-us2
+//public id
+//080841f0fc
